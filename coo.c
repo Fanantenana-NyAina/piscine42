ce9f7dc8b4786150aa7c8595f60f7b6026e15d24
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-10-13 09:07:41 +0300

    first approch

---
+++ coo.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+
+void	write_char(char c)
+{
+	write(1, &c, 1);
+}
+
+void	write_two_digits(int n)
+{
+	char tens = n / 10 + '0';
+	char units = n % 10 + '0';
+
+	write_char(tens);
+	write_char(units);
+}
+
+void	display_combinations(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			write_two_digits(i);
+			write_char(' ');
+			write_two_digits(j);
+
+			if (!(i == 98 && j == 99))
+			{
+				write_char(',');
+				write_char(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	write_char('$'); // simulate cat -e end of line
+	write_char('\n');
+}
+
+int	main(void)
+{
+	display_combinations();
+	return (0);
+}
